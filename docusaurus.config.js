1561379764ac4066b67adfa6910ad7ee3ca1a6eb
Ain Nuraini <ainni99.com>
2025-09-27 19:51:57 +0700

    first commit

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -10,8 +10,8 @@ import {themes as prismThemes} from 'prism-react-renderer';
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: 'Docs as code by Ain Nuraini',
+  tagline: 'Documentation as code',
   favicon: 'img/favicon.ico',
 
   // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
@@ -83,7 +83,7 @@ const config = {
         respectPrefersColorScheme: true,
       },
       navbar: {
-        title: 'My Site',
+        title: 'Docs as code',
         logo: {
           alt: 'My Site Logo',
           src: 'img/logo.svg',
